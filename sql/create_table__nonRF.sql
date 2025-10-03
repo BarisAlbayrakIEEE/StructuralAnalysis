7fb06d8923f86955563b53e3c5688efba8b32fd7
BarisAlbayrakIEEE <baris.albayrak.ieee@gmail.com>
2025-10-03 15:24:48 +0300

    readme

--- sql/create_table__nonRF.sql
+++ sql/create_table__nonRF.sql
@@ -8,7 +8,10 @@ CREATE TABLE EO_Panel (
 CREATE TABLE SC_Stifener (
     SC_Stifener_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(99) NOT NULL UNIQUE,
-    EO_Stiffener    INT NOT NULL,
-    EO_Panel__side_1 INT NOT NULL,
-    EO_Panel__side_2 INT NOT NULL
+    EO_Stiffener_id     INT NOT NULL,
+    EO_Panel_id__side_1 INT NOT NULL,
+    EO_Panel_id__side_2 INT NOT NULL
+    FOREIGN KEY (EO_Stiffener_id)     REFERENCES EO_Stiffener(EO_Stiffener_id),
+    FOREIGN KEY (EO_Panel_id__side_1) REFERENCES EO_Panel(EO_Panel_id__side_1),
+    FOREIGN KEY (EO_Panel_id__side_2) REFERENCES EO_Panel(EO_Panel_id__side_2),
 );
